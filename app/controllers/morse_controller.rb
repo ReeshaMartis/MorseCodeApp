65eba7bc4c19b4210a5c5536ed650dcb5cde1b7e
ReeshaMartis <reeshamartis@gmail.com>
2025-09-15 13:22:23 -0400

    Adding notifications base structure

--- app/controllers/morse_controller.rb
+++ app/controllers/morse_controller.rb
@@ -5,7 +5,19 @@ class MorseController < ApplicationController
   def about
   end
   def encode
+    if params[:text].present?
+      flash.now[:notice] = "Encoded successfully!"
+    else
+      flash.now[:alert] = "Please enter text to encode."
+    end
   end
-  def decode
+
+ def decode
+  if params[:text].present?
+    flash.now[:notice] = "Decoded successfully!"
+  else
+    flash.now[:alert] = "Please enter Morse code to decode."
   end
+end
+
 end
